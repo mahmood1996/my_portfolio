68262603a60e39169321bcccd537dcd8dc87bad5
mahmood1996 <mahmood.abdelrazek@outlook.com>
2026-08-08 04:25:19 +0300

    Added cover images for bespoke coffee and bespoke merchant

--- lib/features/home/src/presentation/widgets/home_view.dart
+++ lib/features/home/src/presentation/widgets/home_view.dart
@@ -70,7 +70,7 @@ final class _HomeViewState extends State<HomeView> {
 
     if (targetKey != null && targetKey.currentContext != null) {
       Scrollable.ensureVisible(
-        alignment: 1.0,
+        alignment: 0.8,
         targetKey.currentContext!,
         duration: const Duration(milliseconds: 600),
         curve: Curves.easeInOut,
